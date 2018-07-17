803a6e0ac0e097ed2d88b714b855a2193dd2a397
james <944137090@qq.com>
2018-07-17 11:26:21 +0800

    Initial commit

---
+++ testProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testProject
+//
+//  Created by Dynamsoft on 17/07/2018.
+//  Copyright © 2018 Dynamsoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
